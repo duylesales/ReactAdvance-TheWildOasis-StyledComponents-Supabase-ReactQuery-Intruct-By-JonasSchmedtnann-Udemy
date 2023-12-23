40d4a94292474b3a4c7ceeb50e36985f74c1e942
Duy Node <duyle.sales@gmail.com>
2023-12-23 13:06:34 +0700

    Fetching Cabin Data w/t slaleTime

--- src/App.jsx
+++ src/App.jsx
@@ -10,10 +10,22 @@ import Account from "./pages/Account";
 import Login from "./pages/Login";
 import PageNotFound from "./pages/PageNotFound";
 import AppLayout from "./ui/AppLayout";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+
+const queryClient = new QueryClient ({
+  defaultOptions:{
+    queries:{
+      // staleTime:60 * 1000,
+      staleTime:0,
+    }
+  }
+})
 
 export default function App() {
   return (
-    <>
+    <QueryClientProvider client={queryClient}>
+      <ReactQueryDevtools initialIsOpen={false}/>
       <BrowserRouter>
         <GlobalStyles />
         <Routes>
@@ -31,6 +43,6 @@ export default function App() {
           <Route path="*" element={<PageNotFound />} />
         </Routes>
       </BrowserRouter>
-    </>
+      </QueryClientProvider>
   );
 }
